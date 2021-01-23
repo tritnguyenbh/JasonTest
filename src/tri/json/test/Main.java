af09c911ddde6e3453f39fd8c96bd27e27c0ce25
Tri Nguyen <tritnguyen2512@gmail.com>
2021-01-22 16:16:07 -0800

    Add second person.

--- src/tri/json/test/Main.java
+++ src/tri/json/test/Main.java
@@ -6,6 +6,8 @@ public class Main {
 
     public static void main(String[] args) {
         Person p = new Person("Tom", "Ngo", 33, "Black");
+        Person p2 = new Person("Trump", "Dump", 77, "Red");
         System.out.println(p);
+        System.out.println(p2);
     }
 }
