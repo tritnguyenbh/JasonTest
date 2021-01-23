defefe7c26502797623f17c5d05254baab08f4e6
Tri Nguyen <vn51fb6@homeoffice.wal-mart.com>
2021-01-22 16:27:38 -0800

    Change Main.java

--- src/tri/json/test/Main.java
+++ src/tri/json/test/Main.java
@@ -6,6 +6,8 @@ public class Main {
 
     public static void main(String[] args) {
         Person p = new Person("Tom", "Ngo", 33, "Black");
+        Person newP = new Person("Gong", "Cha", 41, "Green");
         System.out.println(p);
+        System.out.println(newP);
     }
 }
